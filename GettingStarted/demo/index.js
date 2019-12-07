1028b40c88c1010e6abaa8bed494d32ab51e1ac7
2399131676@qq.com <45560874+liz-wsx@users.noreply.github.com>
2019-12-07 10:04:18 +0800

    20191207-1
    修改了线的样式和行为

--- GettingStarted/demo/index.js
+++ GettingStarted/demo/index.js
@@ -4,6 +4,37 @@
 
 	var graph = new joint.dia.Graph;
 
+	joint.dia.Link.define('interface.CustomLink', {
+		attrs: {
+			line: {
+				connection: true,
+				strokeWidth: 2,
+				strokeLinejoin: 'round'
+			},
+			wrapper: {
+				connection: true,
+				strokeWidth: 10,
+				strokeLinejoin: 'round'
+			}
+		}
+	}, {
+		markup: [{
+			tagName: 'path',
+			selector: 'wrapper',
+			attributes: {
+				'fill': 'none',
+				'cursor': 'pointer',
+				'stroke': 'transparent'
+			}
+		}, {
+			tagName: 'path',
+			selector: 'line',
+			attributes: {
+				'fill': 'none',
+				'pointer-events': 'none'
+			}
+		}]
+	});
 
 	var paper = new joint.dia.Paper({
 		width: 1000,
@@ -12,10 +43,10 @@
 		drawGrid: true,
 		model: graph, // Set graph as the model for paper
 		defaultLink: function(elementView, magnet) {
-			return new joint.shapes.standard.Link({
+			return new joint.shapes.interface.CustomLink({
 				attrs: {
 					line: {
-						stroke: 'white'
+						stroke: '#717d98'
 					}
 				}
 			});
@@ -37,6 +68,27 @@
 		cursor: 'grab'
 	});
 }
+
+function changeReferenceToConstraint(link) {
+	link.attr({
+		line: {
+			targetMarker: {
+				'type': 'path',
+				'd': 'M 10 -5 0 0 10 5 z'
+			}
+		}
+	});
+}
+
+function changeInterfaceToReference(link) {
+	link.attr({
+		line: {
+			strokeDasharray: '5 5',
+			strokeDashoffset: '2.5'
+		}
+	});
+}
+
 
 //中间的框框
 {
@@ -157,8 +209,8 @@ var stencil = new joint.ui.Stencil({
 		step3: {
 			index: 3,
 			label: '3. add interfaces',
-			closed:true,
-			height:1/3
+			closed: true,
+			height: 1 / 3
 		},
 		step4: {
 			index: 4,
@@ -168,12 +220,12 @@ var stencil = new joint.ui.Stencil({
 		step5: {
 			index: 5,
 			label: '5. add references & constraint',
-			height:1/3
+			height: 1 / 3
 		},
-		step6:{
-			index:6,
-			height:297,
-			label:'  '
+		step6: {
+			index: 6,
+			height: 297,
+			label: '  '
 		}
 
 	},
@@ -188,19 +240,27 @@ var stencil = new joint.ui.Stencil({
 graph.on('add', function(cell, collection, opt) {
 	var models = paper.model.attributes.cells.models;
 	if (opt.stencil && cell.attributes.type == 'machine.entity') {
-		for(i=0;i<models.length-1;i++)
-		{
-			if(models[i].attributes.type == 'machine.entity') {
-				joint.ui.FlashMessage.open('Machine already existed!','',{type:'alert',closeAnimation:{delay:3000}});
+		for (i = 0; i < models.length - 1; i++) {
+			if (models[i].attributes.type == 'machine.entity') {
+				joint.ui.FlashMessage.open('Machine already existed!', '', {
+					type: 'alert',
+					closeAnimation: {
+						delay: 3000
+					}
+				});
 				cell.remove();
 				break;
 			}
 		}
-	}else if(opt.stencil && cell.attributes.type == 'requirement.entity') {
-		for(i=0;i<models.length-1;i++)
-		{
-			if(models[i].attributes.type == 'requirement.entity') {
-				joint.ui.FlashMessage.open('requirement already existed!','',{type:'alert',closeAnimation:{delay:3000}});
+	} else if (opt.stencil && cell.attributes.type == 'requirement.entity') {
+		for (i = 0; i < models.length - 1; i++) {
+			if (models[i].attributes.type == 'requirement.entity') {
+				joint.ui.FlashMessage.open('requirement already existed!', '', {
+					type: 'alert',
+					closeAnimation: {
+						delay: 3000
+					}
+				});
 				cell.remove();
 				break;
 			}
@@ -218,7 +278,8 @@ stencil.render().load({
 				text: 'machine'
 			},
 			description: {
-				text: null
+				text: null,
+				y: 3
 			},
 			r1: {
 				ref: 'r3',
@@ -264,7 +325,7 @@ stencil.render().load({
 			}
 		}
 	}],
-	step3:[],
+	step3: [],
 	step4: [{
 		type: 'requirement.entity',
 		attrs: {
@@ -286,253 +347,211 @@ stencil.render().load({
 });
 
 // 点击显示的东西和右面的框框
-{
-	paper.on('element:pointerclick', function(elementView) {
-		joint.ui.Inspector.create('.inspector-container', {
-			cell: elementView.model,
-			inputs: {
-				'attrs/label/text': {
-					type: 'text',
-					label: 'Label',
-					group: 'basic',
-					index: 1
-				},
-				level: {
-					type: 'range',
-					min: 1,
-					max: 10,
-					unit: 'x',
-					defaultValue: 6,
-					label: 'Level',
-					group: 'advanced',
-					index: 2
-				}
-			},
-			groups: {
-				basic: {
-					label: 'Basic',
-					index: 1
-				},
-				advanced: {
-					label: 'Advanced',
-					index: 2
-				}
-			}
-		});
-	});
-
-	// 点element显示的东西,还有一个不知道干啥的halo
-	paper.on('element:pointerclick', function(elementView) {
-		var handles = [{
-			name: 'remove',
-			position: 'nw',
-			events: {
-				pointerdown: 'removeElement'
-			}
-		}, {
-			name: 'myCustomAction',
-			position: 'ne',
-			icon: 'data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7'
-		}];
-		if (!elementView.model.hasPorts()) {
-			// Only shapes without ports will have the "link" handle in the Halo control panel. Shapes with ports can be connected by "dragging" ports.
-			handles.push({
-				name: 'link',
-				position: 'e',
-				events: {
-					pointerdown: 'startLinking',
-					pointermove: 'doLink',
-					pointerup: 'stopLinking'
-				}
-			});
-		}
-		var halo = new joint.ui.Halo({
-			cellView: elementView,
-			handles: handles
-		}).render();
-
-		halo.on('action:myCustomAction:pointerdown', function(evt) {
-			alert('My Control Button Clicked!');
-		});
-	});
-
-	// 点link显示link上的tool
-	paper.on('link:pointerup', function(linkView) {
-		paper.removeTools();
-		var toolsView = new joint.dia.ToolsView({
-			name: 'my-link-tools',
-			tools: [
-				new joint.linkTools.Vertices(),
-				new joint.linkTools.SourceArrowhead(),
-				new joint.linkTools.TargetArrowhead(),
-				new joint.linkTools.Segments,
-				new joint.linkTools.Remove({
-					offset: -20,
-					distance: 40
-				})
-			]
-		});
-		linkView.addTools(toolsView);
-	});
-	//点空白处
-	paper.on('blank:pointerdown', function() {
-		paper.removeTools();
-	});
-}
-
-//上面的框框
-{
-	var toolbar = new joint.ui.Toolbar({
-		groups: {
-			clear: {
+paper.on('element:pointerclick', function(elementView) {
+	joint.ui.Inspector.create('.inspector-container', {
+		cell: elementView.model,
+		inputs: {
+			'attrs/label/text': {
+				type: 'text',
+				label: 'Label',
+				group: 'basic',
 				index: 1
 			},
-			zoom: {
+			level: {
+				type: 'range',
+				min: 1,
+				max: 10,
+				unit: 'x',
+				defaultValue: 6,
+				label: 'Level',
+				group: 'advanced',
 				index: 2
 			}
 		},
-		tools: [{
-				type: 'button',
-				name: 'clear',
-				group: 'clear',
-				text: 'Clear Diagram'
+		groups: {
+			basic: {
+				label: 'Basic',
+				index: 1
 			},
-			{
-				type: 'button',
-				name: 'problem',
-				group: 'problem',
-				text: 'Problem Diagram'
-			},
-			{
-				type: 'button',
-				name: 'context',
-				group: 'context',
-				text: 'Context Diagram'
-			},
-			 { type: 'zoom-out', name: 'zoom-out', group: 'zoom' },
-			 { type: 'zoom-in', name: 'zoom-in', group: 'zoom' }
-		],
-		references: {
-			 paperScroller: paperScroller // built in zoom-in/zoom-out control types require access to paperScroller instance
+			advanced: {
+				label: 'Advanced',
+				index: 2
+			}
 		}
 	});
-	toolbar.on({
-		'clear:pointerclick': graph.clear.bind(graph)
-	});
-	toolbar.on('problem:pointerclick', function(event) {
-		showProblemDiagram();
-	});
-	toolbar.on('context:pointerclick', function(event) {
-		showContextDiagram();
-	});
+});
 
-	document.querySelector('.toolbar-container').appendChild(toolbar.el);
-	toolbar.render();
-
-	function showProblemDiagram() {
-		var models = paper.model.attributes.cells.models;
-		for (var i = 0; i < models.length; i++) {
-			if (models[i].attributes.type == "requirement.CustomElement") {
-				models[i].attr('label/visibility', 'hidden');
-				models[i].attr('body/visibility', 'hidden');
-				models[i].attr('button/visibility', 'hidden');
-				models[i].attr('buttonLabel/visibility', 'hidden');
-			} else if (models[i].attributes.type == "reference.CustomLink") { //constraint.CustomLink
-				models[i].attr('line/visibility', 'hidden')
-			} else if (models[i].attributes.type == "constraint.CustomLink") {
-				models[i].attr('line/visibility', 'hidden');
-			}
-		}
-		joint.ui.FlashMessage.open('succeed in problem!', '', {
-			type: 'alert',
-			closeAnimation: {
-				delay: 3000
-			}
-		});
-		return;
-	}
-
-	function showContextDiagram() {
-		var models = paper.model.attributes.cells.models;
-		for (var i = 0; i < models.length; i++) {
-			if (models[i].attributes.type == "requirement.CustomElement") {
-				models[i].attr('label/visibility', 'visible');
-				models[i].attr('body/visibility', 'visible');
-				models[i].attr('button/visibility', 'visible');
-				models[i].attr('buttonLabel/visibility', 'visible');
-			} else if (models[i].attributes.type == "reference.CustomLink") {
-				models[i].attr('line/visibility', 'visible')
-			} else if (models[i].attributes.type == "constraint.CustomLink") {
-				models[i].attr('line/visibility', 'visible');
-			}
-		}
-		joint.ui.FlashMessage.open('succeed in context!', '', {
-			type: 'alert',
-			closeAnimation: {
-				delay: 3000
-			}
-		});
-		return;
+function getTypeById(id) {
+	var models = paper.model.attributes.cells.models;
+	for (i = 0; i < models.length; i++) {
+		if (models[i].id == id) return models[i].attributes.type;
 	}
 }
 
+function getLabelById(id) {
+	var models = paper.model.attributes.cells.models;
+	for (i = 0; i < models.length; i++) {
+		if (models[i].id == id) return models[i].attrs.label.text;
+	}
+}
 
-// 没用过的功能
-// {
-// 	var myShape = new joint.shapes.machine.entity({
-// 		size: {
-// 			width: 100,
-// 			height: 100
-// 		},
-// 		position: {
-// 			x: 50,
-// 			y: 50
-// 		},
-// 		attrs: {
-// 			label: {
-// 				text: 'My machine'
-// 			}
-// 		},
-// 	});
-// 	graph.addCell(myShape);
+// 点element显示的东西,还有一个不知道干啥的halo
+paper.on('element:pointerclick', function(elementView) {
+	var handles = [{
+		name: 'remove',
+		position: 'nw',
+		events: {
+			pointerdown: 'removeElement'
+		}
+	}];
+	handles.push({
+		name: 'link',
+		position: 'e',
+		events: {
+			pointerdown: 'startLinking',
+			pointermove: 'doLink',
+			pointerup: 'stopLinking'
+		}
+	});
+	var halo = new joint.ui.Halo({
+		cellView: elementView,
+		handles: handles
+	}).render();
 
-// 	// Get element from the graph and change its properties.
-// 	myShape = graph.getElements()[0];
-// 	myShape.prop('attrs/label/text', 'My Updated Shape');
-// 	myShape.prop('size/width', 150);
-// 	myShape.prop('level', 2);
-// 	myShape.prop('attrs/body/fill', '#FE854F');
+	halo.on('action:link:add', function(link) {
+			var sourceId = link.get('source').id;
+			var targetId = link.get('target').id;
+			var sourceType = getTypeById(sourceId);
+			var targetType = getTypeById(targetId);
 
-// 	// Create a clone of an element.
-// 	var myShape2 = myShape.clone();
-// 	myShape2.translate(400, 0);
-// 	graph.addCell(myShape2);
+			if (!sourceId || !targetId || (sourceType == 'domain.entity' && targetType == 'domain.entity') || (sourceType ==
+					'machine.entity' && targetType == 'requirement.entity') || (sourceType == 'requirement.entity' && targetType ==
+					'machine.entity')) {
+				link.remove();
+			} else if ((sourceType ==
+					'domain.entity' && targetType == 'requirement.entity') || (sourceType == 'requirement.entity' && targetType ==
+					'domain.entity'))
+					{
+						changeInterfaceToReference(link);
+					}
+	});
+});
 
-// 	// Create a link that connects two elements.
-// 	var myLink = new joint.shapes.standard.Link({
-// 		attrs: {
-// 			line: {
-// 				stroke: 'white'
-// 			}
-// 		},
-// 		source: {
-// 			id: myShape.id,
-// 			port: 'out1'
-// 		},
-// 		target: {
-// 			id: myShape2.id,
-// 			port: 'in1'
-// 		}
-// 	});
-// 	graph.addCell(myLink);
+// 点link显示link上的tool
+paper.on('link:pointerup', function(linkView) {
+	paper.removeTools();
+	var toolsView = new joint.dia.ToolsView({
+		name: 'my-link-tools',
+		tools: [
+			new joint.linkTools.Remove({
+				offset: -20,
+				distance: 40
+			})
+		]
+	});
+	linkView.addTools(toolsView);
+});
+//点空白处
+paper.on('blank:pointerdown', function() {
+	paper.removeTools();
+});
 
-// 	// React on changes in the graph.
-// 	// graph.on('change add remove', function() {
-// 	// 	var diagramJsonString = JSON.stringify(graph.toJSON());
-// 	// 	console.log('Diagram JSON', diagramJsonString);
-// 	// });
-// 	graph.on('change:level', function(cell, level) {
-// 		var color = (level > 8) ? 'red' : 'white';
-// 		cell.prop('attrs/body/fill', color);
-// 	});
-// }
+//上面的框框
+var toolbar = new joint.ui.Toolbar({
+	groups: {
+		clear: {
+			index: 1
+		},
+		zoom: {
+			index: 2
+		}
+	},
+	tools: [{
+			type: 'button',
+			name: 'clear',
+			group: 'clear',
+			text: 'Clear Diagram'
+		},
+		{
+			type: 'button',
+			name: 'problem',
+			group: 'problem',
+			text: 'Problem Diagram'
+		},
+		{
+			type: 'button',
+			name: 'context',
+			group: 'context',
+			text: 'Context Diagram'
+		},
+		{
+			type: 'zoom-out',
+			name: 'zoom-out',
+			group: 'zoom'
+		},
+		{
+			type: 'zoom-in',
+			name: 'zoom-in',
+			group: 'zoom'
+		}
+	],
+	references: {
+		paperScroller: paperScroller // built in zoom-in/zoom-out control types require access to paperScroller instance
+	}
+});
+toolbar.on({
+	'clear:pointerclick': graph.clear.bind(graph)
+});
+toolbar.on('problem:pointerclick', function(event) {
+	showProblemDiagram();
+});
+toolbar.on('context:pointerclick', function(event) {
+	showContextDiagram();
+});
+
+document.querySelector('.toolbar-container').appendChild(toolbar.el);
+toolbar.render();
+
+function showProblemDiagram() {
+	var models = paper.model.attributes.cells.models;
+	for (var i = 0; i < models.length; i++) {
+		if (models[i].attributes.type == "requirement.CustomElement") {
+			models[i].attr('label/visibility', 'hidden');
+			models[i].attr('body/visibility', 'hidden');
+			models[i].attr('button/visibility', 'hidden');
+			models[i].attr('buttonLabel/visibility', 'hidden');
+		} else if (models[i].attributes.type == "reference.CustomLink") { //constraint.CustomLink
+			models[i].attr('line/visibility', 'hidden')
+		} else if (models[i].attributes.type == "constraint.CustomLink") {
+			models[i].attr('line/visibility', 'hidden');
+		}
+	}
+	joint.ui.FlashMessage.open('succeed in problem!', '', {
+		type: 'alert',
+		closeAnimation: {
+			delay: 3000
+		}
+	});
+}
+
+function showContextDiagram() {
+	var models = paper.model.attributes.cells.models;
+	for (var i = 0; i < models.length; i++) {
+		if (models[i].attributes.type == "requirement.CustomElement") {
+			models[i].attr('label/visibility', 'visible');
+			models[i].attr('body/visibility', 'visible');
+			models[i].attr('button/visibility', 'visible');
+			models[i].attr('buttonLabel/visibility', 'visible');
+		} else if (models[i].attributes.type == "reference.CustomLink") {
+			models[i].attr('line/visibility', 'visible')
+		} else if (models[i].attributes.type == "constraint.CustomLink") {
+			models[i].attr('line/visibility', 'visible');
+		}
+	}
+	joint.ui.FlashMessage.open('succeed in context!', '', {
+		type: 'alert',
+		closeAnimation: {
+			delay: 3000
+		}
+	});
+}
